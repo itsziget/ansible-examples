84833694a3af266969dccfb397c1ce52e173cbb0
Takács Ákos <rimelek@rimelek.hu>
2019-08-25 12:43:13 +0200

    move ssh private key parameter of the first example into the inventory file

--- examples/02-ad-hoc-with-inventory/run.sh
+++ examples/02-ad-hoc-with-inventory/run.sh
@@ -4,5 +4,4 @@ ansible \
     remotes \
     --inventory inventory.yml \
     --module-name raw \
-    --args 'ls -la /' \
-    --private-key '../../.vagrant/machines/default/virtualbox/private_key'
+    --args 'ls -la /'
