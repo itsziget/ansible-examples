6cf19a34d7cec50a687ccb344b2473fc8cc5f608
Takács Ákos <rimelek@rimelek.hu>
2019-09-08 17:31:20 +0200

    fix ansible fact cache path

--- examples/10-playbook-variable-declaration/run.sh
+++ examples/10-playbook-variable-declaration/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ANSIBLE_CACHE_PLUGIN=jsonfile
-export ANSIBLE_CACHE_PLUGIN_CONNECTION=/tmp/ansible-cache.json
+export ANSIBLE_CACHE_PLUGIN_CONNECTION="/tmp/ansible-cache$(cd "$(dirname "$0")" && pwd)/$(basename "$0")"
 
 ansible-playbook \
     playbook.yml \
