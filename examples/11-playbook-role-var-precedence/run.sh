b6669d81c88c915d212cc2cf9912d7ff708fd874
Takács Ákos <rimelek@rimelek.hu>
2019-09-08 21:11:46 +0200

    add example to demonstrate variable precedence

---
+++ examples/11-playbook-role-var-precedence/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export ANSIBLE_CACHE_PLUGIN=jsonfile
+export ANSIBLE_CACHE_PLUGIN_CONNECTION="/tmp/ansible-cache$(cd "$(dirname "$0")" && pwd)/$(basename "$0")"
+
+if [[ -f "${ANSIBLE_CACHE_PLUGIN_CONNECTION}" ]]; then
+  unlink "${ANSIBLE_CACHE_PLUGIN_CONNECTION}"
+fi
+
+ansible-playbook \
+  playbook-cache-fact.yml \
+  --inventory inventory/default/inventory.yml
+
+ansible-playbook \
+    playbook.yml \
+    --inventory inventory/default/inventory.yml \
+    --extra-vars 'location="22. extra vars"'
+    < <(echo)
