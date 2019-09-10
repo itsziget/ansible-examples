0aff50da9af07705fa92080d57148211f6295001
Takács Ákos <rimelek@rimelek.hu>
2019-09-10 21:39:14 +0200

    fix cleaning the cache

--- examples/11-playbook-role-var-precedence/run.sh
+++ examples/11-playbook-role-var-precedence/run.sh
@@ -3,8 +3,10 @@
 export ANSIBLE_CACHE_PLUGIN=jsonfile
 export ANSIBLE_CACHE_PLUGIN_CONNECTION="/tmp/ansible-cache$(cd "$(dirname "$0")" && pwd)/$(basename "$0")"
 
-if [[ -f "${ANSIBLE_CACHE_PLUGIN_CONNECTION}" ]]; then
-  unlink "${ANSIBLE_CACHE_PLUGIN_CONNECTION}"
+# Just for testing
+# Do not remove the cache before each run in production
+if [[ -d "${ANSIBLE_CACHE_PLUGIN_CONNECTION}" ]]; then
+  rm -rf "${ANSIBLE_CACHE_PLUGIN_CONNECTION}"
 fi
 
 ansible-playbook \
